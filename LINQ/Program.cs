0dda25fa7357bd026d6478c943820f73e5e78424
Will Royal <william.j.royal@gmail.com>
2023-11-11 21:04:48 -0800

    initial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>() { "Red Dead Redemption 2, Grand Theft Auto V, Crash Bandicoot" };
+
+            videoGames.Add("SOCOM US Navy Seals");
+
+            videoGames.Add("Madden NFL");
+
+            var orderedGames = videoGames.OrderBy(name => name.Length);
+
+            foreach(var game in orderedGames)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
